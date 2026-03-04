b2a96c5539441d1eaec08cdf6f69a468a2ca8db6
Vinicius Charles <143054545+V-Charles@users.noreply.github.com>
2026-03-03 22:15:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -10,8 +10,16 @@ const funcionarios = [
   
   funcionarios.forEach(funcionario => {
     const itemDaLista = document.createElement('li');
-
     itemDaLista.textContent = `${funcionario.nome} trabalha como ${funcionario.cargo} e ganha R$${funcionario.salario}`;
-    
     listaNoHTML.appendChild(itemDaLista);
   });
+  
+  const listaVipHTML = document.getElementById('lista-vip');
+  
+  funcionarios
+    .filter(funcionario => funcionario.salario > 5000)
+    .forEach(funcionario => {                          
+      const itemDaLista = document.createElement('li');
+      itemDaLista.textContent = `🎯 VIP: ${funcionario.nome} - ${funcionario.cargo} (R$ ${funcionario.salario})`;
+      listaVipHTML.appendChild(itemDaLista);
+    });
